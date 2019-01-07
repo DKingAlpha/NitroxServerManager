d3b753466aba92be485e80c2175c2ffb0717277b
DKing <dkingchn@gmail.com>
2019-01-07 20:00:45 +0800

    better unique server dict

--- NitroxServerManager.py
+++ NitroxServerManager.py
@@ -1,9 +1,10 @@
 import os
 import sys
-import web
 import time
 import threading
 import pickle
+import socket
+import web
 
 
 # work in script dir
@@ -41,6 +42,7 @@ class notify:
         auth_serverinfo, uniq_server_dict, keepalive = load_data()
 
         i_serverstr = serverinfo.rsplit('|', 1)[0]
+        i_serveraddr = i_serverstr.rsplit(':', 1)[1]
         i_playernum = serverinfo.rsplit('|', 1)[1]
         if op == 'online':
             if pingerid in auth_serverinfo:
@@ -48,19 +50,19 @@ class notify:
                 o_playernum = auth_serverinfo[pingerid].rsplit('|', 1)[1]
                 if i_serverstr == o_serverstr:
                     auth_serverinfo[pingerid] = serverinfo
-                    uniq_server_dict[i_serverstr] = pingerid
+                    uniq_server_dict[i_serveraddr] = pingerid
                     keepalive[pingerid] = time.time()
                     save_data(auth_serverinfo, uniq_server_dict, keepalive)
                     return "Server info updated"
                 else:
                     return "Server info rejected"
             else:
-                if i_serverstr in uniq_server_dict:
+                if i_serveraddr in uniq_server_dict:
                     return "Wrong key to update server info! If this is your server, it will recover in next update"
                 else:
                     if len(pingerid) == 32:
                         auth_serverinfo[pingerid] = serverinfo
-                        uniq_server_dict[i_serverstr] = pingerid
+                        uniq_server_dict[i_serveraddr] = pingerid
                         keepalive[pingerid] = time.time()
                         save_data(auth_serverinfo, uniq_server_dict, keepalive)
                         return "Server info registered"
@@ -69,7 +71,7 @@ class notify:
         if op == 'offline':
             if pingerid in auth_serverinfo:
                 auth_serverinfo.pop(pingerid)
-                uniq_server_dict.pop(serverinfo.rsplit('|', 1)[0])
+                uniq_server_dict.pop(i_serveraddr)
                 keepalive.pop(pingerid)
                 save_data(auth_serverinfo, uniq_server_dict, keepalive)
                 return "Server info removed"
@@ -89,7 +91,7 @@ def clean_outdated_server():
                 serverinfo = auth_serverinfo.pop(id)
                 uniq_server_dict.pop(serverinfo.rsplit('|', 1)[0])
                 keepalive.pop(id)
-                print("Removing " + serverinfo)
+                print(serverinfo + " has not updated since 10 minutes ago. Removing it.")
         save_data(auth_serverinfo, uniq_server_dict, keepalive)
 
 
@@ -100,5 +102,7 @@ class MyApplication(web.application):
 
 if __name__ == "__main__":
     app = MyApplication(urls, globals())
-    threading.Thread(target=clean_outdated_server).start()
+    th = threading.Thread(target=clean_outdated_server)
+    th.daemon = True
+    th.start()
     app.run(port=80)
